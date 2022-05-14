7588c89bb6aab2ffcd0ff0a86f615b6d614b6208
seungwan Ham <seungwan7511@gmail.com>
2022-05-15 00:10:56 +0900

    handle image uploading

--- src/app.jsx
+++ src/app.jsx
@@ -4,14 +4,14 @@ import styles from './app.module.css';
 import Login from './components/login/login';
 import Maker from './components/maker/maker';
 
-function App({ authService }) {
+function App({ FileInput, authService }) {
   return (
     <div class={styles.app}>
       <BrowserRouter>
         <Routes>
           <Route path='/' exact element={<Login authService={authService} />} />
           <Route path='/login' element={<Login authService={authService} />} />
-          <Route path='/maker' element={<Maker authService={authService} />} />
+          <Route path='/maker' element={<Maker FileInput={FileInput} authService={authService} />} />
         </Routes>
       </BrowserRouter>
     </div>
